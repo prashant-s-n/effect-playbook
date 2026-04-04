c219cfddfa705367876e88c216f0457ae7b626dd
Prashant <prashantk19372@gmail.com>
2026-04-04 22:29:26 +0530

    Pricing data added in product fixture

--- chapters/test-driven-contracts/fixture.ts
+++ chapters/test-driven-contracts/fixture.ts
@@ -10,4 +10,15 @@ export const products: ProductCase[] = [
       name: "Trail Mix",
     },
   ],
+  [
+    "a simple product with a pricing",
+    {
+      id: "c7b1c6eb-a76b-417b-89f9-75fd3cca1dea",
+      name: "Trail Mix",
+      price: {
+        amount: "12",
+        currency: "usd",
+      },
+    },
+  ],
 ];
